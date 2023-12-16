b5a9645b2560b2c29363408eca935a0b2b9b2314
Danielooo <dvanraffe@gmail.com>
2023-12-16 17:56:56 +0100

    change 1 checkout

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
             <h1>Git changes</h1>
             <p>change 1</p>
             <i>subchange of 1</i>
+            <i>little change of 1 to be able to push</i>
         
         </>
     );
